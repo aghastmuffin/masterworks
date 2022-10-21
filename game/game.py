24e9b4a82dd1cecc08c8c5499872597afb060ad4
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2022-10-20 18:31:36 -0700

    test

--- game/game.py
+++ game/game.py
@@ -1,1 +1,28 @@
-
+import pygame
+#we can add a verify account with discord for highscore/better web functions
+pygame.init()
+class player(pygame.sprite.Sprite):
+    def __init__(self, color, height, width):
+        super().__init__()
+        #self.image will eventually be an image, but for now its a surface for a square
+        self.image = pygame.Surface([width, height])
+        self.image.fill(SURFACE_COLOR)
+        self.image.set_colorkey(COLOR)
+  
+        pygame.draw.rect(self.image,color,pygame.Rect(0, 0, width, height))
+  
+        self.rect = self.image.get_rect()
+    def kmove():
+        x = 0
+        y = 0
+        dist = 5
+        #dist is movement speed.
+        key = pygame.key.get_pressed()
+        if key[pygame.K_w]:
+            y = y - dist
+        if key[pygame.K_a]:
+            x = x - dist
+        if key[pygame.K_s]:
+            y = y + dist
+        if key[pygame.K_d]:
+            x = x + dist 
