e9eac7dd7802cd8102f556968318ea358f9d557d
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2022-11-06 17:43:58 -0800

    Update game.py

--- game/game.py
+++ game/game.py
@@ -1,10 +1,39 @@
+
 import pygame
-#we can add a verify account with discord for highscore/better web functions
+from pygame import mixer, font
+from random import randint
+import os
+"""
+Copyright © 2022 Kingve
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+#limit backward movement, or store old variable pos in x and use scroll like that
+# GLOBAL VARIABLES
 pygame.init()
-class player(pygame.sprite.Sprite):
+font.init()
+RED = (255, 0, 0)
+GREEN = (47, 120, 1)
+COLOR = (255, 100, 98)
+SURFACE_COLOR = (167, 255, 100)
+WIDTH = 500
+HEIGHT = 500
+GREEN = (17, 186, 11)
+LEVEL = (0)
+jumping = 0
+dash = 1
+score = 0
+precalrect = 0
+curx = 0
+# Object class
+class Sprite(pygame.sprite.Sprite):
     def __init__(self, color, height, width):
         super().__init__()
-        #self.image will eventually be an image, but for now its a surface for a square
+  
         self.image = pygame.Surface([width, height])
         self.image.fill(SURFACE_COLOR)
         self.image.set_colorkey(COLOR)
@@ -12,27 +41,136 @@ class player(pygame.sprite.Sprite):
         pygame.draw.rect(self.image,color,pygame.Rect(0, 0, width, height))
   
         self.rect = self.image.get_rect()
-    def kmove():
-        x = 0
-        y = 0
-        dist = 5
-        #dist is movement speed.
+    def outsidecam(self):
+        #
+        rectprex = self.rect.x
+        #redifining GLOBAL variabes
+        if rectprex < 0:
+            #randomly add at right side of screen
+            self.rect.x()
+            return
+        else:
+            print("in")
+    def ad(self, prex):
+        global x
+        global precalrect
+        dist = 2
         key = pygame.key.get_pressed()
-        if key[pygame.K_w]:
-            y = y - dist
-        if key[pygame.K_a]:
-            x = x - dist
-        if key[pygame.K_s]:
-            y = y + dist
         if key[pygame.K_d]:
-            x = x + dist 
+            #instead of moving the player we make another keyboard for platform
+
+            x = x - dist 
+        if key[pygame.K_a]:
+            x = x + dist
+
+#we only want the x cord once, then we add to it We could try lists
+#        precalrect = self.rect.x
+#instead of defining the x in the function (which causes sliding), we instead ask the sprite to provide their spawn x cord, which does cause a little more jumble, but in might actually finally please work.
+        precalrect = prex
+        self.rect.x = precalrect + x
+    def keyboard(self):
+        key = pygame.key.get_pressed()
+        dist = 5
+        #defining globals
+        global y
+        global x 
+        global gravset
+        global gravity2
+        global jumping
+        global dash
+        global score
+        gravset = 1
+        dev = 0
+        if dev:
+            if key[pygame.K_w]:
+                y = y - dist
+                gravset = 0
+                gravity2 = 0
+            if key[pygame.K_s]:
+                #for s we make it so the platforms move, instead of the player
+                y = y + dist
+        if key[pygame.K_x]:
+            if jumping:
+                if dash == 1: 
+                    x = x + 80
+                    dash = 0
+                #make more balanced also cooldown, but overall dash is a good idea
+            else:
+                dash = 1
+        if key[pygame.K_SPACE]:
+            score = score + 1
+            if jumping == 0:
+                jumping = 1
+                y = y - dist
+#                gravset = 0
+                gravity2 = 4
+#160, 500
+        collide1 = pygame.Rect.colliderect(self.rect, platform.rect)
+        collide2 = pygame.Rect.colliderect(self.rect, platform1.rect)
+        collide = 0 + collide1 + collide2
+        if gravset == 1:
+            #implies to python that collide = 1, so if you're touching any platform it will return a one if none are being touched it gives a 0
+            if collide:
+                #if we touch then execute:
+                jumping = 0
+                dash = 1
+            else:
+                #if we aren't currently touching execute:
+                gravity2 = gravity2 - 0.1
+                #slowly build up gravity, then add the added gravity to the player's movement
+                y = y - gravity2
+#respawn loop, if you exit the screen, then you are automatically put back to the start of the level
+        if y > 500:
+            x = 0
+            y = 0
+            gravity2 = 0
+#            object_.rect.x = x
+            object_.rect.y = y
+        object_.rect.y = y
+  
+size = (WIDTH, HEIGHT)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("SPLAT - A platformer")
+all_sprites_list = pygame.sprite.Group() 
+#define sprites
+object_ = Sprite(RED, 20, 25)
+all_sprites_list.add(object_)
+platform = Sprite(GREEN, 20, 25)
+platformx = 0
+platform.rect.x = platformx
+platform.rect.y = 100
+all_sprites_list.add(platform)
+platform1 = Sprite(GREEN, 10, 25)
+platform1x = randint(40, 70)
+platform1.rect.x = platform1x
+platform1.rect.y = randint(20, 70)
+all_sprites_list.add(platform1)
+y = int(0)
+x = int(0)
+gravity2 = 0
+
+gravity2 = 0
+exit = True
+clock = pygame.time.Clock()
+gravset = 1
+
+def varrun():
+    object_.keyboard()
+    platform.outsidecam()
+    platform.ad(prex=platformx)
+    platform1.ad(prex=platform1x)
+    
 while exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = False
+    varrun()
+#    object_.rect.x = x
+    object_.rect.y = y
     all_sprites_list.update()
     screen.fill(SURFACE_COLOR)
     all_sprites_list.draw(screen)
     pygame.display.flip()
     clock.tick(60)
   
+pygame.quit()
